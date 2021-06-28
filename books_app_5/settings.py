a73af1d15c76802a39731b3a99d4bf303429c593
aquesidilly <aquesidilly@yahoo.com>
2021-06-28 17:43:22 +0000

    books_app_5

--- books_app_5/settings.py
+++ books_app_5/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
     'products',
+    'bag',
 ]
 
 MIDDLEWARE = [
@@ -71,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents'
             ],
         },
     },
