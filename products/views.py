4547af6fc1e3db663f7df99afe97e9bf0003850b
aquesidilly <aquesidilly@yahoo.com>
2021-06-30 22:15:12 +0000

    books_app_5

--- products/views.py
+++ products/views.py
@@ -56,7 +56,7 @@ def  all_products(request):
     context = {
         'products': products,
         'search':query,
-        'current_categories':categories,
+        'current_categories': categories,
         'current_sorting': current_sorting,
     }
 
